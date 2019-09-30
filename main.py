5ae5442438b23c2f7b7e852ad3d4a82f407bf193
david98 <davidevolta98@gmail.com>
2019-09-30 13:47:30 +0200

    Removed print statements

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import requests
 from requests.auth import HTTPDigestAuth
-import shlex
 from bs4 import BeautifulSoup
 
 
@@ -10,9 +9,9 @@ class Rio:
         self.ip = ip
         self.port = port
         self.session = requests.session()
-        self.action_id = '12345'
-        self.directory = '/.syscfg-action/12345/systemimage.tar.gz'
-        self.file_name = 'systemimage.tar.gz'
+        self.action_id = None
+        self.directory = None
+        self.file_name = None
 
     def reboot(self, mode: int = 0):
         response = self.session.post(f'http://{self.ip}:{str(self.port)}/rtexecsvc/RebootEx', files={}, data={'RebootIn': 0, 'BootMode': mode})
@@ -32,7 +31,6 @@ class Rio:
                                     data=open(file_name, 'rb'),
                                     auth=HTTPDigestAuth('admin', ''))
         self.file_name = file_name
-        print(response.content)
 
     def set_system_image(self):
         if self.action_id is None or self.directory is None:
@@ -44,21 +42,18 @@ class Rio:
                                            'Options': '8204', 'LocalPath': f'{self.directory}/{self.file_name}'},
                                      files={})
         soup = BeautifulSoup(response.content.decode('utf-8'), 'xml')
-        print(response.status_code)
-        print(response.content)
         elements = soup.findAll('SetSystemImage')
         if elements is None or len(elements) == 0 or elements[0].get('Result') != '0':
             raise Exception('Error while setting system image.')
 
     def get_progress(self):
         response = self.session.get(f'http://{self.ip}:{str(self.port)}/siws/Progress')
-        print(response.content)
 
 
 if __name__ == "__main__":
     rio = Rio('172.16.3.7', 80)
-    result = rio.reboot(0)
-    #directory = rio.begin_action('{02CF21F5-820E-FF87-A8D9-A504FCFE9558}')
-    #rio.put_file('systemimage.tar.gz')
-    #rio.set_system_image()
-    #rio.get_progress()
+    result = rio.reboot(1)
+    directory = rio.begin_action('{02CF21F5-820E-FF87-A8D9-A504FCFE9558}')
+    rio.put_file('systemimage.tar.gz')
+    rio.set_system_image()
+    rio.get_progress()
